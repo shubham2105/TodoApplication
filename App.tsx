bbb58201ac923148f3bdc494c6b67e57fdfc927e
shubham2105 <shubhamdhole215@gmail.com>
2025-08-24 12:57:46 +0530

    Added Dark Mode implementation

--- App.tsx
+++ App.tsx
@@ -1,45 +1,15 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { Text} from 'react-native'
+import React from 'react'
+import './global.css'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
-
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
+const App = () => {
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
+    <SafeAreaView className="flex-1 items-center justify-center bg-white dark:bg-black">
+      <Text className="font-semibold text-3xl text-black dark:text-white">TodoApplication</Text>
+    </SafeAreaView>
+  )
 }
 
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
+export default App
 
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
-
-export default App;
